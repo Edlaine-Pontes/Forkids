ddb9d39121abd585f262611dd3dbd9e8bd5dd413
Edlaine Pontes <edlaine.psico@gmail.com>
2020-12-11 22:33:55 -0300

    Correção da rota

--- src/app.js
+++ src/app.js
@@ -10,9 +10,10 @@ database.connect()
 app.use(cors())
 app.use(express.json())
 
+app.use('/estabelecimentos', estabelecimentosRoute)
+
 app.use('/', (req, res) => {
     res.send('Eu estou vivo!')
 })
-app.use('/estabelecimentos', estabelecimentosRoute)
 
 module.exports = app
