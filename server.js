5d3382467c943d7dd0d76294014604b344b66e8b
Edlaine Pontes <edlaine.psico@gmail.com>
2020-12-11 22:22:35 -0300

    Ajustes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv-safe').config()
 
 const app = require('./src/app')
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando http://localhost:${PORT}`)
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-// const bodyParser = require('body-parser')
+
 const estabelecimentosRoute = require('./routes/estabelecimentoRoutes')
 
 const database = require('./configs/database')
@@ -10,6 +10,9 @@ database.connect()
 app.use(cors())
 app.use(express.json())
 
+app.use('/', (req, res) => {
+    res.send('Eu estou vivo!')
+})
 app.use('/estabelecimentos', estabelecimentosRoute)
 
 module.exports = app
